bc77ab82cd66813598045975fa4e717fc368c092
Unknown <gioatf.rotta@gmail.com>
2020-02-28 02:45:00 -0300

    Change db

--- config/default.js
+++ config/default.js
@@ -11,7 +11,7 @@ module.exports = {
     port: process.env.PORT || 8000,
     timezone: 'America/Sao_Paulo',
     db: {
-      uri: 'mongodb://heroku_470l5p0n:g8afr7c31a8oci4dei1cphce7l@ds337418.mlab.com:37418/heroku_470l5p0n'
+      uri: 'mongodb://heroku_whmrx1z4:9gsqkhhps9aq3p31jlu6ub2cln@ds223379-a0.mlab.com:23379,ds223379-a1.mlab.com:23379/heroku_whmrx1z4?replicaSet=rs-ds223379'
     },
     facebook: {
       secret: '3f19a398f049644fcc5fd1e8fc0889ce',
