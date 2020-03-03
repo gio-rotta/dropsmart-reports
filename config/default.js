66a3cbd3053ff97bb23d431407a9dd3c01654692
Unknown <gioatf.rotta@gmail.com>
2020-03-02 22:11:40 -0300

    Db retry writes false

--- config/default.js
+++ config/default.js
@@ -11,7 +11,7 @@ module.exports = {
     port: process.env.PORT || 8000,
     timezone: 'America/Sao_Paulo',
     db: {
-      uri: 'mongodb://heroku_whmrx1z4:9gsqkhhps9aq3p31jlu6ub2cln@ds223379-a0.mlab.com:23379,ds223379-a1.mlab.com:23379/heroku_whmrx1z4?replicaSet=rs-ds223379'
+      uri: 'mongodb://heroku_whmrx1z4:9gsqkhhps9aq3p31jlu6ub2cln@ds223379-a0.mlab.com:23379,ds223379-a1.mlab.com:23379/heroku_whmrx1z4?replicaSet=rs-ds223379&retryWrites=false'
     },
     facebook: {
       secret: '3f19a398f049644fcc5fd1e8fc0889ce',
